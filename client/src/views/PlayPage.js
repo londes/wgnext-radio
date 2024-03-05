bab466dca8edc95c9d21fe3ebbd6f47889061f36
William Purcell <wedozeez@gmail.com>
2024-03-05 16:47:37 -0500

    just added some basic stuff to the play page

--- client/src/views/PlayPage.js
+++ client/src/views/PlayPage.js
@@ -4,8 +4,17 @@ import { tracks } from '../data/tracks'
 
 export default function PlayPage() {
   return (
-    <div className="player-container">
-        {tracks.map(track => <audio src={track.src} controls />)}
+    <div className="player-song-container">
+        {tracks.map(track => <div className='track-container'>
+            <div>{track.title} by {track.artist}</div>
+            <audio src={track.src} controls />
+        </div>)}
+        {/* <div className="song-container">
+            {tracks.map(track => <div>{track.title}, {track.artist}</div>)}
+        </div>
+        <div className="player-container">
+            {tracks.map(track => <audio src={track.src} controls />)}
+        </div> */}
     </div>
   )
 }
